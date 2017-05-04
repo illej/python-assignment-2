d011e058c862667e5353938ac748bb1e1aec679b
Elliot <elliot.ayrey@gmail.com>
2017-05-04 16:26:11 +1200

    4may17: smell1-step3.5-consolidate conditional expression

--- visualiser.py
+++ visualiser.py
@@ -4,59 +4,22 @@ import pygal
 # TODO: would be nice to build titles in a separate function!
 class Visualiser(object):
     def __init__(self):
-        self.__funcs = {'-b': lambda x: self.display_bar(x),
-                        '-l': lambda x: self.display_line(x),
-                        '-p': lambda x: self.display_pie(x),
-                        '-r': lambda x: self.display_radar(x)}
-        # self.__objs = {'-b': BarVisualiser(),
-        #                 '-l': BarVisualiser(),
-        #                 '-p': BarVisualiser(),
-        #                 '-r': BarVisualiser()}
+        self.__charts = {'-b': pygal.Bar(),
+                         '-l': pygal.Line(),
+                         '-p': pygal.Pie(),
+                         '-r': pygal.Radar()}
 
-    def display_chart(self, input_param, data_set_dict):
-        self.__funcs[input_param](data_set_dict)
-        # self.__objs[input_param].display('x')
+    def display_chart(self, arg, data):
+        obj = self.__charts[arg]
+        chart = self.build_chart(obj, data)
+        chart.render_in_browser()
 
-    def display_bar(self, data_dict):
+    def build_chart(self, obj, data):
+        chart = obj
         title_builder = []
-        title = ""
-        bar_chart = pygal.Bar()
-        for key, data_list in data_dict.items():
+        for key, data_list in data.items():
             title_builder.append(key + ' vs ')
-            title = ''.join(title_builder)
-            bar_chart.add(key, data_list)
-        bar_chart.title = title[:-4]
-        bar_chart.render_in_browser()  # opens default web browser
+            chart.add(key, data_list)
+            chart.title = ''.join(title_builder)[:-4]
+        return chart
 
-    def display_line(self, data_dict):
-        title_builder = []
-        title = ""
-        line_chart = pygal.Line()
-        for key, data_list in data_dict.items():
-            title_builder.append(key + ' vs ')
-            title = ''.join(title_builder)
-            line_chart.add(key, data_list)
-        line_chart.title = title[:-4]
-        line_chart.render_in_browser()
-
-    def display_pie(self, data_dict):
-        title_builder = []
-        title = ""
-        pie_chart = pygal.Pie()
-        for key, data_list in data_dict.items():
-            title_builder.append(key + ' vs ')
-            title = ''.join(title_builder)
-            pie_chart.add(key, data_list)
-        pie_chart.title = title[:-4]
-        pie_chart.render_in_browser()
-
-    def display_radar(self, data_dict):
-        title_builder = []
-        title = ""
-        radar_chart = pygal.Radar()
-        for key, data_list in data_dict.items():
-            title_builder.append(key + ' vs ')
-            title = ''.join(title_builder)
-            radar_chart.add(key, data_list)
-        radar_chart.title = title[:-4]
-        radar_chart.render_in_browser()
